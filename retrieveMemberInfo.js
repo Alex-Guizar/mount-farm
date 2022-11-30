0675999ed08aad3c549f622c587bdd2ea3fefc48
Alex Guizar <alexguizar90@gmail.com>
2022-11-29 19:17:57 -0800

    Added the ability to update mount data from api.

--- retrieveMemberInfo.js
+++ retrieveMemberInfo.js
@@ -1,15 +1,6 @@
-const schedule = require('node-schedule');
-const express = require('express');
+//const schedule = require('node-schedule');
 const fs = require('fs');
 const axios = require('axios');
-const app = express(),
-      bodyParser = require("body-parser");
-      port = 3080;
-
-// place holder for the data
-const users = [
-  
-];
 
 async function retrieveFcData() {
   const response = await axios.get('https://xivapi.com/freecompany/9229142273877455434?data=FCM');
@@ -91,67 +82,8 @@ function updateMembers() {
   });
 }
 
-app.use(bodyParser.json());
-app.use(express.static(process.cwd() + '/my-app/dist'));
-
 updateMembers();
 
 // schedule.scheduleJob("* */5 * * * *", function() {
 //   updateMembers();
 // });
-
-app.get('/api/updateMembers', (req, apiRes) => {
-  console.log('Update Members!');
-  const fcMembers = [];
-  const fcData = retrieveFcData();
-  fcData.then(res => {
-    //console.log(res);
-    res.FreeCompanyMembers.forEach((member) => {
-      const fcMember = {};
-      fcMember.name = member.Name;
-      fcMember.ID = member.ID;
-      fcMembers.push(fcMember);
-    });
-
-    const finished = new Promise((resolve, reject) => {
-      let i = 0;
-      setTimeout(function run() {
-        const memberData = retrieveCharData(fcMembers[i].ID);
-        memberData.then(res => {
-          console.log(res.Character.Name);
-          fcMembers[i].Mounts = res.Mounts;
-          if (i < fcMembers.length - 1) {
-            i++;
-            setTimeout(run, 1000);
-          } else {
-            resolve(true);
-          }
-        })
-        .catch(err => {
-          console.error(err);
-          reject(err);
-        });
-      }, 1000);
-    });
-
-    finished.then((res) => {
-      console.log('API Calls Finished!');
-      //console.log(fcMembers);
-      const jsonContent = JSON.stringify(fcMembers);
-      fs.writeFile('./src/json/MemberInfo.json', jsonContent, err => {
-        if (err) {
-          console.error(err);
-        }
-
-        console.log("Users Saved!");
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  })
-  .catch(err => {
-    console.log(err);
-  });
-  apiRes.json("Users Updating!");
-});
---
+++ retrieveMountInfo.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const axios = require('axios');
+
+const mountNames = [
+	// A Realm Reborn
+	{ exp: "ARR", name: "aithon", from: "trial" },
+	{ exp: "ARR", name: "xanthos", from: "trial" },
+	{ exp: "ARR", name: "gullfaxi", from: "trial" },
+	{ exp: "ARR", name: "enbarr", from: "trial" },
+	{ exp: "ARR", name: "markab", from: "trial" },
+	{ exp: "ARR", name: "boreas", from: "trial" },
+	// Heavensward
+	{ exp: "HW", name: "lanner", from: "trial" },
+	{ exp: "HW", name: "gobwalker", from: "raid" },
+	{ exp: "HW", name: "arrhidaeus", from: "raid" },
+	// Stormblood
+	{ exp: "STB", name: "kamuy", from: "trial" },
+	{ exp: "STB", name: "alte roite", from: "raid" },
+	{ exp: "STB", name: "air force", from: "raid" },
+	{ exp: "STB", name: "model o", from: "raid" },
+	// Shadowbringers
+	{ exp: "SHB", name: "gwiber", from: "trial" },
+	{ exp: "SHB", name: "skyslipper", from: "raid" },
+	{ exp: "SHB", name: "ramuh", from: "raid" },
+	{ exp: "SHB", name: "eden", from: "raid" },
+	// Endwalker
+	{ exp: "EW", name: "lynx", from: "trial" },
+	{ exp: "EW", name: "demi-phoinix", from: "raid" },
+	{ exp: "EW", name: "sunforged", from: "raid" },
+];
+
+const blackList = [
+	"wondrous lanner",
+	"kamuy of the nine tails",
+];
+
+function buildData(mountData) {
+	const modifiedData = mountData.map((mount) => {
+		const expansion = mountNames.find((mName) => mount.Name.toLowerCase().includes(mName.name.toLowerCase()));
+		mount.Icon = `https://xivapi.com${mount.Icon}`;
+		if (expansion) {
+			mount.Expansion = expansion.exp;
+			mount.From = expansion.from;
+		}
+		return mount;
+	});
+	
+	return modifiedData;
+}
+
+async function retrieveMountData() {
+	let matchArray = [];
+	mountNames.forEach((mount) => {
+		const shouldObj = {
+			"wildcard": {
+				"NameCombined_en": "*"+ mount.name +"*"
+			}
+		};
+
+		matchArray.push(shouldObj);
+	});
+
+	const mountParams = {
+		"indexes": "mount",
+  	"columns": "Name,ID,Icon",
+		"body": {
+			"query": {
+				"bool": {
+					"minimum_should_match": 1,
+					"should": matchArray
+				}
+			},
+			"from": 0,
+			"size": 50
+		},
+	}
+
+  const response = await axios.post('https://xivapi.com/search', mountParams);
+
+  if (!response.statusText) {
+		const message = `An error has occured: ${response.status}`;
+
+		throw new Error(message);
+	}
+
+	const mountData = await response.data.Results;
+	console.log('API Calls Finished!');
+	return mountData;
+}
+
+function sortMounts(a, b) {
+	if ((a.From === "trial" && b.From === "raid") && a.Expansion === b.Expansion) {
+		return -1;
+	}
+	if ((a.From === "raid" && b.From === "trial") && a.Expansion === b.Expansion) {
+		return 1;
+	}
+	return 0;
+}
+
+function updateMounts() {
+  const mountData = retrieveMountData();
+  mountData.then(res => {
+    //console.log(res);
+		const filteredArr = res.filter((mount) => !blackList.includes(mount.Name.toLowerCase()));
+		const finishedData = buildData(filteredArr);
+		finishedData.sort(sortMounts);
+    const jsonContent = JSON.stringify(finishedData);
+		
+		fs.writeFile('./src/json/TrackedMounts.json', jsonContent, err => {
+			if (err) {
+				console.error(err);
+			}
+
+			console.log("Mounts Saved!");
+		});
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+updateMounts();
